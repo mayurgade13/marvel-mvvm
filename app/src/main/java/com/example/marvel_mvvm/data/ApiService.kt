51f2915993b6b3805d9e774191e55d39c378946d
Mayur Gade <mgade@Mayurs-Mac-mini.local>
2022-12-17 19:42:23 +0530

    consumed-marvel-api

---
+++ app/src/main/java/com/example/marvel_mvvm/data/ApiService.kt
@@ -0,0 +1,23 @@
+package com.example.marvel_mvvm.data
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    companion object {
+        const val GET_MARVEL_CHARACTERS = "/v1/public/characters"
+
+        // Keys
+        const val MARVEL_API_TIMESTAMP = "ts"
+        const val MARVEL_API_KEY = "apikey"
+        const val MARVEL_API_HASH = "hash"
+    }
+
+    @GET(GET_MARVEL_CHARACTERS)
+    suspend fun getMarvelCharacters(
+        @Query(MARVEL_API_TIMESTAMP) timestamp: String,
+        @Query(MARVEL_API_KEY) apiKey: String,
+        @Query(MARVEL_API_HASH) hash: String
+    )
+}
